35f8923b1f759c3524f9d3a4d0645d810813da33
Illy Illy <Teh404gal@proton.me>
2024-04-15 16:14:00 +0100

    revert my bullshit

--- includes.container/deb-pkgs/install-debs.sh
+++ includes.container/deb-pkgs/install-debs.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -e
-#this will only help me but screw it
-apt-get install -y /deb-pkgs/*.deb
 
-#for file in /deb-pkgs/*; do
-#    if [ ! -f "$file" ]; then
-#        continue
-#    fi
-#
-#    extension=${file##*.}
-#    if [ $extension != "deb" ]; then
-#        continue
-#    fi
-#
-#    echo "installing $file"
-#
-#    apt-get install -y $file
-#done
+for file in /deb-pkgs/*; do
+    if [ ! -f "$file" ]; then
+        continue
+    fi
+
+    extension=${file##*.}
+    if [ $extension != "deb" ]; then
+        continue
+    fi
+
+    echo "installing $file"
+
+    apt-get install -y $file
+done
